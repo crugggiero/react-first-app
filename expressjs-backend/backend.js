f12de013a3e0c09d0d2b272d7105f49e623ab3a5
crugggiero <ruggicar555@gmail.com>
2022-01-16 16:25:52 -0800

    added axios

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 5000
+const cors = require('cors');
 
 const users = { 
     users_list :
@@ -33,7 +34,8 @@ const users = {
     ]
  }
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,12 +1,21 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Table from './Table'
-import Form from './Form';
+import Form from './Form'
+import axios from 'axios'
+
 
 
 function MyApp() {
 
   const [characters, setCharacters] = useState([]);
   
+  useEffect(() => {
+    fetchAll().then( result => {
+      if(result)
+        setCharacters(result);
+    });
+  }, [] );
+
   function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
         return i !== index
@@ -15,6 +24,16 @@ function MyApp() {
   }
   function updateList(person) {
     setCharacters([...characters, person]);
+  }
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;
+    } catch (error) {
+      console.log(error);
+      return false
+    }
   }
 
   return (
